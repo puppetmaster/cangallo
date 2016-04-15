e587cfffbd6f53f3eb551e948b4fc7cecec6fe1f
Javi Fontan <jfontan@gmail.com>
2016-04-15 17:32:47 -0500

    Starting the new cangafile format

--- lib/cangallo/cangafile.rb
+++ lib/cangallo/cangafile.rb
@@ -12,9 +12,34 @@ class Cangallo
   class Cangafile
     attr_accessor :data
 
+    ACTIONS = {
+      "copy" => lambda do |input|
+        required = %w{source destination}
+        if (required & input.keys) != required
+          raise %s{copy command needs "source" and "destination"}
+        end
+
+        "copy-in #{input["source"}:#{input["destination"]}"
+      end,
+
+      "run" => lambda do |input|
+        if input.class != String
+          raise %s{run command needs a string}
+        end
+
+        "run-command #{input}"
+      end
+    }
+
     def initialize(file)
       text = File.read(file)
       @data = YAML.load(text)
+
+      if !@data["tasks"] || @data["tasks"].class != Array
+        raise "No tasks defined or it's not an array"
+      end
+
+      @tasks = @data["tasks"]
     end
 
     def file_commands
@@ -43,6 +68,16 @@ class Cangallo
     end
 
     def libguestfs_commands
+      @tasks.each do |task|
+        if task.class != Hash
+          raise "This task is not a hash: #{task}"
+        end
+
+
+      end
+    end
+
+    def libguestfs_commands_old
       text = file_commands
       text << run_commands
       text
